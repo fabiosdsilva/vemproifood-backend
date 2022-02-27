a00a5cebf8e3eee5ad25f094a363024b3737c4ff
fabiosdsilva <fabiosds17@gmail.com>
2022-02-27 05:25:50 -0300

    Working playlist finder

--- src/services/spotify/getApiSpotify.ts
+++ src/services/spotify/getApiSpotify.ts
@@ -1,16 +1,37 @@
-import axios from "axios";
+import SpotifyWebApi from "spotify-web-api-node";
+import scopes from "./scopes";
 
 class ApiSpotify {
 
-   async getPlayList(name?: string) {
+    protected readonly clientId = '4507ee0b3b24462fadb8d10a868b2cee';
+    protected readonly clientSecret = process.env.SECRET;
+    public spotifyApi;
+
+    constructor() {
+        this.spotifyApi = new SpotifyWebApi({
+            clientId: this.clientId,
+            clientSecret: this.clientSecret,
+            redirectUri: 'http://localhost:8081'
+        });
+    };
+
+    public getToken() {
+        const result = this.spotifyApi.createAuthorizeURL(scopes);
+        console.log(result);
+        return result;
+    }
+
+    public async getPlayList(name?: string) {
        try {
-           const pushData = await axios({
-               method: 'get',
-               url: 'https://api.spotify.com/v1/playlists/',
-               responseType: "json",
-           })
+           const playlist = await this.spotifyApi.getPlayList(name);
+
+           if (playlist) {
+               return playlist;
+           }
        } catch (e) {
-           
-       }
-   }
-}
+           return e;
+       };
+   };
+};
+
+export default new ApiSpotify();
