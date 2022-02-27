d1be758c4da615bb5f12e048c8bef6e131631b8c
fabiosdsilva <fabiosds17@gmail.com>
2022-02-26 22:29:46 -0300

    Acess to wetahermap.org and return datas

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
+import {parse, stringify, toJSON, fromJSON} from 'flatted';
 import axios from "axios";
 
 type WetaherMap = string | number;
 
 class Index {
    async index(req: Request, res: Response) {
+       // City, Lon, Lag
        try {
            const wetaherMap: WetaherMap = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=campinas&appid=b77e07f479efe92156376a8b07640ced');
-
            if (wetaherMap) {
-               return res.json({ wetaherMap });
+               return res.json(wetaherMap.data);
            };
        } catch (e) {
            const errror: string[] = [`${e}`];
