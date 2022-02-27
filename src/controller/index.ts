37b7e663b8ad56c5bb99b63d265c8c1cffb9a007
fabiosdsilva <fabiosds17@gmail.com>
2022-02-27 20:56:53 -0300

    Funciona... mas não mostra as playlists

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from "express";
 
-import ApiWeatherMap from "../services/weathermap/apiWeathermap";
+import ApiWeatherMap from "../services/weathermap/getApiWeathermap";
+import ApiSpotiyWeb from "../services/spotify/getApiSpotify";
 
 type WetaherMap = string | number;
 
 class Index {
    async index(req: Request, res: Response) {
-       // City, Lon, Lag
        try {
-          const result: WetaherMap = await ApiWeatherMap.index() ;
-          return res.json(result);
+          const result: WetaherMap = await ApiWeatherMap.index();
+
+          return res.json(await ApiSpotiyWeb.getPlayList('workout'));
 
        } catch (e) {
            const errror: string[] = [`${e}`];
            return res.status(401).json({ errror });
        };
+   };
+
+   async login(req: Request, res: Response) {
+       return res.redirect(ApiSpotiyWeb.getToken());
    };
 };
 
