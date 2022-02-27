6ec1bd447234a395a7d75822910602bb8360e92c
fabiosdsilva <fabiosds17@gmail.com>
2022-02-27 05:49:53 -0300

    Add rout login-spotify

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
 import index from "../controller/index";
-const routes = Router();
 
+const routes = Router();
 // Router
 routes.get('/', index.index);
+
+// Spotify
+routes.get('/spotify', index.spotify);
+
+routes.get('/login', index.login);
+
 
 export default routes;
