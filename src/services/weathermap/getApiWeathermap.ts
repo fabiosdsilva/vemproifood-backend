2131e36c1ca4eadb0a78cfa1c5beb660387939c1
fabiosdsilva <fabiosds17@gmail.com>
2022-02-28 18:19:28 -0300

    Key do openweather agora é private

--- src/services/weathermap/getApiWeathermap.ts
+++ src/services/weathermap/getApiWeathermap.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 
 class ApiWeatherMap {
 
-    public key = process.env.KEY_WEATHER;
+    private key = process.env.KEY_WEATHER;
 
     async index(city?: string, lon?: string, lat?: string) {
         try {
-            const getTemp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=campinas&appid=${this.key}`)
+            const getTemperature = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=campinas&appid=${this.key}`)
 
-            if (getTemp) {
-                return getTemp.data;
+            if (getTemperature) {
+                return getTemperature.data;
             };
 
         } catch (e) {
