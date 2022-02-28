84cbac3156e8cdb82c2620fc82e128d5e5054c34
fabiosdsilva <fabiosds17@gmail.com>
2022-02-28 18:18:33 -0300

    Consigo gerar o token e pesquisar por playlist

--- src/services/spotify/getApiSpotify.ts
+++ src/services/spotify/getApiSpotify.ts
@@ -3,31 +3,31 @@ import scopes from "./scopes";
 
 class ApiSpotify {
 
-    protected readonly clientId = '4507ee0b3b24462fadb8d10a868b2cee';
-    protected readonly clientSecret = process.env.SECRET;
+    private clientId = '4507ee0b3b24462fadb8d10a868b2cee';
+    private clientSecret = process.env.SECRET;
     public spotifyApi;
 
     constructor() {
         this.spotifyApi = new SpotifyWebApi({
             clientId: this.clientId,
             clientSecret: this.clientSecret,
-            redirectUri: 'http://localhost:8081'
+            redirectUri: 'http://localhost:8081/'
         });
+        this.getToken();
     };
 
     public getToken() {
         const result = this.spotifyApi.createAuthorizeURL(scopes);
         console.log(result);
+        
         return result;
     }
 
     public async getPlayList(name?: string) {
        try {
-           const playlist = await this.spotifyApi.getPlayList(name);
+           const playlist = await this.spotifyApi.searchPlaylists(name);
+           return playlist;
 
-           if (playlist) {
-               return playlist;
-           }
        } catch (e) {
            return e;
        };
